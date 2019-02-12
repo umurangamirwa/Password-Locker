641733443c3cb868c880c83d34ca03e1a2156a20
Iyerikuzwe Regine <iyerikuzweregine19@gmail.com>
2019-02-12 09:38:23 +0200

    add user correction

--- user.py
+++ user.py
@@ -3,7 +3,7 @@ class User:
     Class that generates new instances of users.
     """
 
-    user_list = [] # Empty contact list
+    user_list = [] # Empty user list
 
     def __init__(self,username,email,password):
 
